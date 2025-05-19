d1565d5b5227024e7fa6af4b3a81a128074ae519
jatin <jatinhati851@gmail.com>
2025-05-20 01:25:55 +0530

    Final

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,9 @@
+// Project: Word Count 
+// Description: This program reads a line of text from
+//              the user input and counts the number of words
+//              using a reusable function from the internal module.
+
+
 package main
 
 import (
@@ -12,8 +18,8 @@ func main() {
 	fmt.Print("Enter a line of text: ")
 	scanner := bufio.NewScanner(os.Stdin)
 	if scanner.Scan() {
-		input := scanner.Text()                       
-		count := line.CountWords(input)             
+		input := scanner.Text()
+		count := line.CountWords(input)
 		fmt.Printf("Word count: %d\n", count)
 	}
 }
